f59d49b73200e6f7b9969c7ef099b49e96d4a02d
sonnguyenngoc <sonnn@hoangkhang.com.vn>
2016-12-21 00:04:56 -0800

    fix archive and unarchive

--- app/models/erp/taxes/tax.rb
+++ app/models/erp/taxes/tax.rb
@@ -65,11 +65,11 @@ module Erp::Taxes
     end
     
     def self.archive_all
-			update_all(archived: false)
+			update_all(archived: true)
 		end
     
     def self.unarchive_all
-			update_all(archived: true)
+			update_all(archived: false)
 		end
   end
 end
