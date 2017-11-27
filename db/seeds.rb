2ef64228172ce0f8072b1227d0a71ce63d3ec07c
Luan Pham <luanpm88@gmail.com>
2017-11-27 16:05:36 +0700

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ Erp::Taxes::Tax.all.destroy_all
 
 taxes.each_with_index do |t,index|
     Erp::Taxes::Tax.create(
-        name: 'SO-' + taxes[index][0],
+        name: taxes[index][0],
         short_name: taxes[index][1],
         scope: Erp::Taxes::Tax::TAX_SCOPE_SALES,
         computation: Erp::Taxes::Tax::TAX_COMPUTATION_PRICE,
@@ -16,7 +16,7 @@ end
 
 taxes.each_with_index do |t,index|
     Erp::Taxes::Tax.create(
-        name: 'PO-' + taxes[index][0],
+        name: taxes[index][0],
         short_name: taxes[index][1],
         scope: Erp::Taxes::Tax::TAX_SCOPE_PURCHASES,
         computation: Erp::Taxes::Tax::TAX_COMPUTATION_PRICE,
